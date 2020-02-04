8576bc90fa2f869a05d04e428dc856215a5e3eb9
jimmyb2508 <james.barrington1985@gmail.com>
2020-02-04 20:31:52 +0000

    First Challenge

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const artistControllers = require('./controllers/artists');
+
 app.get('*', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
 });
